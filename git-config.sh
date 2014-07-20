38c6822a81507fc569cacad7cb80b0b300d62498
Youssef EL MASMOUDI <y.masmoudi@gmail.com>
2014-07-20 14:55:35 +0300

    update git-config script

--- git-config.sh
+++ git-config.sh
@@ -10,9 +10,36 @@ git config --global color.ui "true"
 cd ~
 curl -OL https://github.com/git/raw/master/contrib/completion/git-completion.bash
 mv ~/git-completion.bash ~/.git-completion.bash
-echo -e "\n if [ -f ~/.git-completion.bash ]; then
+echo -e "\nif [ -f ~/.git-completion.bash ]; then
     source  ~/.git-completion.bash 
 fi;\n" >> ~/.bash_profile
 cd $OLDPWD
 
+# PS1 git configuration
+echo -e "export PS1='\W$(__git_ps1 "(%s)") > '\n" >> ~/bash_profile
 
+# git alias
+#git log --stat --summary  // global statistics about the diff
+#git log --oneline
+#git log -p sha1.. file.c
+#git log --graph --all --decorate
+#git ls-tree
+#git diff --staged
+#git checkout -b // create un checkout
+#git diff branch1..branch2
+#git branch --merged // list of branches merged in the current one
+#git branch -m old new // rename a branch
+
+#git merge branch
+#fast-forward merge => no changes in master 
+#true merge => master changed
+
+#git merge --abort //in case of conflict
+#git stash -save "comment"
+#git stash list
+#git stash show -p name_of_stash
+#git stash pop name //pop it out of list
+#git stash apply name //keep a copy in the list
+#git stash drop
+#git stash clear //drop all stashes
+# git branch -u // to track the branch 
